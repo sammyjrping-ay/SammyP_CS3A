5077c58a0b97e9615ddc6cf8de45586a02674f6b
Sammy Jr. <sammyjrpingay@gmail.com>
2025-03-26 22:15:22 +0800

    commit

--- MT/CS3A_MTExam_Ping-ay/js/feature.js
+++ MT/CS3A_MTExam_Ping-ay/js/feature.js
@@ -1,41 +1,50 @@
+// Cart Array 
 const cart = {};
+
+// Paragraph element for total cost 
 const pTotal = document.getElementById('total');
 
+// Function to add product object in cart 
 function addToCart(productName, productPrice) {
-    if (cart[productName]) {
+    if (cart[productName]) {  // Check if product exist
         cart[productName].quantity += 1;
         cart[productName].totalPrice += productPrice;
     } else {
-        cart[productName] = {
+        cart[productName] = {  // Add product to cart
             quantity: 1,
             totalPrice: productPrice,
             price: productPrice
         };
     }
-    
     updateCartDisplay();
 }
 
+// Function to remove or reduce product object in cart 
 function removeFromCart(productName, productPrice) {
-    if (cart[productName] && cart[productName].quantity > 0) {
-        cart[productName].quantity -= 1; 
+    if (cart[productName] && cart[productName].quantity > 0) {  
+        cart[productName].quantity -= 1;   // Decrease product quantity and total price
         cart[productName].totalPrice -= productPrice;
 
-        if (cart[productName].quantity == 0) {
+        if (cart[productName].quantity == 0) {  // Remove the product from the cart
             delete cart[productName];
         }
     } else {
-        alert('The item is not in the cart!');
+        alert('The item is not in the cart!');  // Fallback code 
     }
     updateCartDisplay();
 }
 
+// Function to render or update the cart list 
 function updateCartDisplay() {
+    // Retrieve the cart list 
     const cartList = document.getElementById('cart');
     cartList.innerHTML = ''; 
-    var total = 0;
+
+    var total = 0;  // Variable for overall total cost
+
     for (let product in cart) {
         
+        // Create elements for each products 
         const divProduct = document.createElement('div');
             divProduct.className = 'cart-product';
         const divTopCard = document.createElement('div');
@@ -52,17 +61,19 @@ function updateCartDisplay() {
         const pTotalPrice = document.createElement('p');
             pTotalPrice.innerText = 'Total Price:  ' + cart[product].totalPrice.toFixed(2);
 
+        // Append elements to the main div 
         divProduct.appendChild(divTopCard);
         divProduct.appendChild(productImg);
         divProduct.appendChild(pQuantity);
         divProduct.appendChild(pPrice);
         divProduct.appendChild(pTotalPrice);
 
+        // Append the main div to cart list
         cartList.appendChild(divProduct); 
         total += cart[product].totalPrice
     }
     
+    // Append paragraph element for total cost
     pTotal.innerText = "Total Cost: Php " + total.toFixed(2);
-
     cartList.appendChild(pTotal);
 }
